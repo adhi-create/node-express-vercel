9f41f3c368d6235f21f75b56024ad29e621e9cc2
adhi <108246061+adhi-create@users.noreply.github.com>
2022-10-16 10:19:01 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "1",
-    massages: "The app is working properly!",
+    message: "The app is working properly!",
   });
 });
 
